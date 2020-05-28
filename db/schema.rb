daddd4bd9abb3420e50e3151230b63e392adec80
jameschjiang13 <monochrome473@gmail.com>
2020-05-27 21:22:11 -0400

    added ocean images and 20 sushi instances to seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_155204) do
+ActiveRecord::Schema.define(version: 2020_05_28_011111) do
 
   create_table "customers", force: :cascade do |t|
     t.string "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_05_26_155204) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "img_url"
   end
 
   create_table "orders", force: :cascade do |t|
